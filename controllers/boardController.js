86d930e2ba5554234b5762e8d952fbb2a2ae020f
jongithub59 <jdelrealj01@gmail.com>
2026-08-10 13:01:31 -0500

    feat: create rough message board structure

--- controllers/boardController.js
+++ controllers/boardController.js
@@ -1,5 +1,23 @@
+const messages = [
+  {
+    text: "The Baxter Society has your number!",
+    user: "Shiv",
+    added: new Date().toDateString(),
+  },
+  {
+    text: "No more games...",
+    user: "Mirage",
+    added: new Date().toDateString(),
+  },
+  {
+    text: "Let's see what this book an do...",
+    user: "Abrams",
+    added: new Date().toDateString(),
+  },
+];
+
 const board_index = (req, res) => {
-  res.render("index");
+  res.render("index", { messages: messages });
 };
 
 module.exports = { board_index };
